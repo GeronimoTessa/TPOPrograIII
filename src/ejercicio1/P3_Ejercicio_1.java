b19be8cdb1b138b8199442040e836bbaf684b0f9
GeronimoTessa <gero.tessa21@gmail.com>
2017-05-16 16:53:56 -0300

    Update P3_Ejercicio_1.java

--- src/ejercicio1/P3_Ejercicio_1.java
+++ src/ejercicio1/P3_Ejercicio_1.java
@@ -3,17 +3,17 @@ package ejercicio1;
 import java.util.List;
 
 public class P3_Ejercicio_1 {
-	
-	public static Soluci髇Ejercicio1 Ejercicio1(List<Punto> puntos){
-		Soluci髇Ejercicio1 respuesta = new Soluci髇Ejercicio1();
+	//Prueba
+	public static Soluci贸nEjercicio1 Ejercicio1(List<Punto> puntos){
+		Soluci贸nEjercicio1 respuesta = new Soluci贸nEjercicio1();
 		if(puntos.size() == 2){
 			respuesta.setPunto1(puntos.get(0));
 			respuesta.setPunto2(puntos.get(1));
 		}else{
 			List<Punto> puntos1 = puntos.subList(0, puntos.size()/2);
 			List<Punto> puntos2 = puntos.subList(puntos.size()/2, puntos.size());
-			Soluci髇Ejercicio1 respuestaPuntos1 = Ejercicio1(puntos1);
-			Soluci髇Ejercicio1 respuestaPuntos2 = Ejercicio1(puntos2);
+			Soluci贸nEjercicio1 respuestaPuntos1 = Ejercicio1(puntos1);
+			Soluci贸nEjercicio1 respuestaPuntos2 = Ejercicio1(puntos2);
 			int distancia1 = modulo(respuestaPuntos1.getPunto1(), respuestaPuntos1.getPunto2());
 			int distancia2 = modulo(respuestaPuntos2.getPunto1(), respuestaPuntos2.getPunto2());
 			if(distancia1 < distancia2){
